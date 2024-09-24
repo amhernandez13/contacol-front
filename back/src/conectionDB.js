426328d7eb84f865df2acd89a72a838e8600b87c
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-09-23 19:49:30 -0500

    DBconnection

---
+++ back/src/conectionDB.js
@@ -0,0 +1,11 @@
+/* import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.URI_DB)
+  .then((data) => {
+    console.log("Success conecting DB");
+  })
+  .catch((error) => {
+    console.log("Error conecting DB");
+  });
+ */
