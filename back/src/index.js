463919156adc43795f2b14db2b55dd04e1ad77f4
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-09-23 21:19:42 -0500

    Filled index, server and connectionDB files

---
+++ back/src/index.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import "./connectionDB.js";
+import server from "./server.js";
+
+// Carga las variables de entorno desde el archivo .env
+dotenv.config();
+server.listen(3000, () => {
+  console.log("Server Port: 3000");
+});
