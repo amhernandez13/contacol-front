380933a752b7556c4920d047e9b6537aed2f85e6
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-09-24 21:59:20 -0500

    Added credentials for cloudinary in connection.js, Added devs package.json

--- back/src/connectionDB.js
+++ back/src/connectionDB.js
@@ -1,17 +1,28 @@
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 
-// Cargar el archivo .env
+// Carga las variables de entorno desde .env
 dotenv.config();
 
-console.log(process.env.MONGO_ENDPOINT);
+// MongoDB connection
 mongoose
   .connect(process.env.MONGO_ENDPOINT)
   .then((data) => {
-    console.log("Success conecting DB");
+    console.log("Success conecting MongoDB");
   })
   .catch((error) => {
-    console.log("Error conecting DB");
+    console.log("Error conecting MongoDB");
+    console.log(error);
   });
+
+// Cloudinary DB connection
+cloudinary.config({
+  cloud_name: "dc6ulxdlx",
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+export default cloudinary; // Exportar con esta linea para usar Cloudinary: import cloudinary from './cloudinaryConfig.js';
 
 /* export default mongoose; */
