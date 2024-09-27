6820ea6782823825c37c99d3080ed673401dcf7c
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-09-26 21:07:06 -0500

    Fill user model, router & controller (CRUD), edited server

---
+++ back/src/controllers/userController.js
@@ -0,0 +1,111 @@
+import bcrypt from "bcryptjs";
+import userModel from "../models/userModel.js";
+
+const userController = {
+  createUser: async (sol, req) => {
+    try {
+      const { name, email, password, role } = sol.body;
+      const protectedPassword = await bcrypt.hash(password, 10);
+      const newUser = new userModel({
+        name,
+        email,
+        password: protectedPassword,
+        role,
+        state: true,
+      });
+      const creatingUser = await newUser.save();
+      if (creatingUser._id) {
+        req.json({
+          state: "Success",
+          mesage: "User created",
+          data: creatingUser._id,
+        });
+      }
+    } catch (error) {
+      req.json({
+        state: "Error",
+        mesage: "Error creating user",
+        data: error,
+      });
+    }
+  },
+
+  readAllUsers: async (sol, req) => {
+    try {
+      const searchingAllUsers = await userModel.find();
+      req.json({
+        state: "Success",
+        mesage: "Users found",
+        data: searchingAllUsers,
+      });
+    } catch (error) {
+      req.json({
+        state: "Error",
+        mesage: "Error finding all users",
+        data: error,
+      });
+    }
+  },
+
+  readUser: async (sol, req) => {
+    try {
+      const searchingUser = await userModel.findById(sol.params.id);
+      if (searchingUser._id) {
+        req.json({
+          state: "Success",
+          mesage: "User found",
+          data: searchingUser,
+        });
+      }
+    } catch (error) {
+      req.json({
+        state: "Error",
+        mesage: "Error finding user",
+        data: error,
+      });
+    }
+  },
+
+  updateUser: async (sol, req) => {
+    try {
+      const updatingUser = await userModel.findByIdAndUpdate(
+        sol.params.id,
+        sol.body
+      );
+      if (updatingUser._id) {
+        req.json({
+          state: "Success",
+          mesage: "User updated",
+          data: updatingUser._id,
+        });
+      }
+    } catch (error) {
+      req.json({
+        state: "Error",
+        mesage: "Error updating user",
+        data: error,
+      });
+    }
+  },
+
+  deleteUser: async (sol, req) => {
+    try {
+      const deletingUser = await userModel.findByIdAndDelete(sol.params.id);
+      if (deletingUser._id) {
+        req.json({
+          state: "Success",
+          mesage: "User deleted",
+          data: null,
+        });
+      }
+    } catch (error) {
+      req.json({
+        state: "Error",
+        mesage: "Error deleting user",
+        data: error,
+      });
+    }
+  },
+};
+
+export default userController;
--- back/src/server.js
+++ back/src/server.js
@@ -1,23 +1,20 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-/* import userRouter from "./routers/userRouter.js";
-import loginRouter from "./routers/loginRouter.js";
-import storageRouter from "./routers/storageRouter.js";
- */
+import userRouter from "./routers/userRouter.js";
+/* import loginRouter from "./routers/loginRouter.js";
+import storageRouter from "./routers/storageRouter.js"; */
 import supplierRouter from "./routers/supplierRouter.js";
-/*
-import invoiceRouter from "./routers/invoiceRouter.js"; // Todos fuera de corchetes porque está exportado por default
-*/
+/* import invoiceRouter from "./routers/invoiceRouter.js"; // Todos fuera de corchetes porque está exportado por default */
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
-/* server.use("/user", userRouter);
-server.use("/login", loginRouter);
-server.use("/invoice", invoiceRouter); 
+server.use("/users", userRouter);
+/* server.use("/login", loginRouter);
+server.use("/invoices", invoiceRouter);
 server.use("/storage", storageRouter); */
 server.use("/suppliers", supplierRouter);
 
