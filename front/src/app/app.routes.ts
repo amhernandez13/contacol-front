9b6f52e3de17c1bdf774f28b0bf4e983e215dee4
Alejandro <garzonortizalejandro@gmail.com>
2024-10-01 17:19:26 -0500

    Se realiza la creacion de los component

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Componentes/home/home.component';
 import { HederComponent } from './Componentes/header/heder.component';
-import { FacturasComponent } from './Componentes/facturas/facturas.component';
 import { LoginComponent } from './Componentes/login/login.component';
 import { ProveedoresComponent } from './Componentes/proveedores/proveedores.component';
 import { RegistroComponent } from './Componentes/registro/registro.component';
@@ -11,7 +10,6 @@ import { UsauriosComponent } from './Componentes/usaurios/usaurios.component';
 export const routes: Routes = [
     {path: 'home', component:HomeComponent},
     {path: 'header', component:HederComponent},
-    {path: 'facturas', component:FacturasComponent},
     {path: 'login', component:LoginComponent},
     {path: 'proveedores', component:ProveedoresComponent},
     {path: 'registro', component:RegistroComponent},
