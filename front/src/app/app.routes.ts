371226d6198dbecc048fecf2f1a9021087ff2b45
amhernandez13 <amhernandez13@outlook.com>
2024-10-13 19:20:53 -0500

    first version of invoice form

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -9,21 +9,14 @@ import { VouchersComponent } from './Componentes/vouchers/vouchers.component';
 import { InvoicesFormComponent } from './Componentes/invoices-form/invoices-form.component';
 import { HederComponent } from './Componentes/header/heder.component';
 
-
-
-
 export const routes: Routes = [
-
-    {path: 'home', component:HomeComponent},  
-    {path: 'login', component:LoginComponent},
-    {path: 'invoice-warehouse', component:InvoiceWarehouseComponent},
-    {path: 'invoices', component:InvoicesComponent},
-    {path: 'suppliers', component:SuppliersComponent},
-    {path: 'users', component:UsersComponent},
-    {path: 'vouchers', component:VouchersComponent},
-    {path: 'invoices', component:InvoicesFormComponent},
-    {path: 'header', component:HederComponent},
-
-
-
-]
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'invoice-warehouse', component: InvoiceWarehouseComponent },
+  { path: 'invoices', component: InvoicesComponent },
+  { path: 'suppliers', component: SuppliersComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'vouchers', component: VouchersComponent },
+  { path: 'invoices-form', component: InvoicesFormComponent },
+  { path: 'header', component: HederComponent },
+];
