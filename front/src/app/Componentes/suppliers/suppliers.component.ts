c39a86da5a8a660801b5bdb996b79212b8832f12
David <david.santiagonino20@gmail.com>
2024-10-15 20:58:13 -0500

    traspoas

--- front/src/app/Componentes/suppliers/suppliers.component.ts
+++ front/src/app/Componentes/suppliers/suppliers.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { SuppliersService } from '../../services/suppliers.service';
 @Component({
   selector: 'app-suppliers',
   standalone: true,
   imports: [],
   templateUrl: './suppliers.component.html',
-  styleUrl: './suppliers.component.css'
+  styleUrl: './suppliers.component.css',
 })
 export class SuppliersComponent {
+  suplier = inject(SuppliersService);
+  adata: any[] = [];
 
+  getallSuppliers() {
+    this.suplier.getSuppliers().subscribe((answer: any) => {
+      console.log('ans: ', answer);
+      if (answer) {
+        this.adata = answer;
+        console.log('Suppliers read correctly');
+      } else {
+        console.log('ups an error has ocurred');
+      }
+      this.adata = answer;
+    });
+  }
+  ngOnInit() {
+    this.getallSuppliers();
+  }
 }
