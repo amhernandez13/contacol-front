ca19355fc71597072657534daeba607619b57423
amhernandez13 <amhernandez13@outlook.com>
2024-10-17 14:33:35 -0500

    removed folders of back and the rest

--- back/src/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import "./connectionDB.js";
-import server from "./server.js";
-
-// Carga las variables de entorno desde el archivo .env
-dotenv.config();
-server.listen(3000, () => {
-  console.log("Server Port: 3000");
-});
